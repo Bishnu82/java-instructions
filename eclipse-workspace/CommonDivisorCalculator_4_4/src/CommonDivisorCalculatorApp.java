a497e071ba819f766f5eaba7230feba527ea3e6d
John Cawley <jcawleyiii@gmail.com>
2019-10-13 22:40:50 -0400

    add project 7-1

---
+++ eclipse-workspace/CommonDivisorCalculator_4_4/src/CommonDivisorCalculatorApp.java
@@ -0,0 +1,47 @@
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+public class CommonDivisorCalculatorApp {
+
+	public static void main(String[] args) {
+		
+System.out.println("Greatest Common Divisor Finder");
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String choice = "y";
+		
+		while (choice.equalsIgnoreCase("y")) {
+			
+			
+			System.out.println("Enter first number:   ");
+			int x = sc.nextInt();
+			System.out.println("Enter second number:   ");
+			int y = sc.nextInt();
+				
+				while (x-y)
+			
+				
+				
+			
+			
+			
+			System.out.println("Loan Ammount:"+"$"+currency.format(ammount));
+			System.out.println("Interest Rate:"+percent.format(iAmmount));
+			System.out.println("Interest:"+"$"+icurrency.format(iTotal));
+		
+			
+			
+			System.out.println("Continue? y/n ");
+			choice = sc.next();
+		
+			
+
+		}
+			sc.close();
+			System.out.println("Bye!");
+	}
+
+}
