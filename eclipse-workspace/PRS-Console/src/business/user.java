358c942b86504ae1958bc51f0b3a4d81f17b3b28
John Cawley <jcawleyiii@gmail.com>
2019-10-23 09:49:28 -0400

    katie try

--- eclipse-workspace/PRS-Console/src/business/user.java
+++ eclipse-workspace/PRS-Console/src/business/user.java
@@ -1,5 +1,115 @@
 package business;
 
 public class user {
+	
+	private int id;     		
+    private String userName;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String email;
+	private int isReviewer;
+    private int isAdmin;
+	
+    public user() {
+		super();
+	}
+
+	public user(int id, String userName, String password, String firstName, String lastName, String phoneNumber,
+			String email, int isReviewer, int isAdmin) {
+		super();
+		this.id = id;
+		this.userName = userName;
+		this.password = password;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.phoneNumber = phoneNumber;
+		this.email = email;
+		this.isReviewer = isReviewer;
+		this.isAdmin = isAdmin;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getIsReviewer() {
+		return isReviewer;
+	}
+
+	public void setIsReviewer(int isReviewer) {
+		this.isReviewer = isReviewer;
+	}
+
+	public int getIsAdmin() {
+		return isAdmin;
+	}
+
+	public void setIsAdmin(int isAdmin) {
+		this.isAdmin = isAdmin;
+	}
+
+	@Override
+	public String toString() {
+		return "user [id=" + id + ", userName=" + userName + ", password=" + password + ", firstName=" + firstName
+				+ ", lastName=" + lastName + ", phoneNumber=" + phoneNumber + ", email=" + email + ", isReviewer="
+				+ isReviewer + ", isAdmin=" + isAdmin + "]";
+	}
+		
+    	
 
 }
+// constraint uname unique (username)
